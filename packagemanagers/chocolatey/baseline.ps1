ab8712b0c12ba9cf196aa31bd2f9cb030ca37018
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2021-11-10 19:20:27 +0100

    Adding choco apps

--- packagemanagers/chocolatey/baseline.ps1
+++ packagemanagers/chocolatey/baseline.ps1
@@ -1,13 +1,27 @@
+# Linux apps
 choco install vim -y
+choco install wget -y
+choco install curl -y
+
 choco install lens -y
 choco install mingw -y
 choco install postman -y
 choco install putty -y
-choco install putty.portable -y
+#choco install putty.portable -y
+choco install putty.install -y
+#choco install notepadplusplus.install -y
+#choco install 7zip.install -y
+#choco install git.install -y
 
-#Visual studio
+choco install sql-server-management-studio -y
+
+# Visual studio
 choco install visualstudio2019professional -y
 choco install vscode -y
+
+# Python
+
+choco install python3 -y
 
 #Java
 #choco install javaruntime -y

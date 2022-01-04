b4292ce0917112b82a95736ac0c2f725f7baac1d
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2022-01-04 20:36:49 +0100

    Consolidating unittest

--- python/TestingLanguage/tests/test.py
+++ python/TestingLanguage/tests/test.py
@@ -1,9 +1,15 @@
 from unittest import TestCase
 
+from python.TestingLanguage.HelloWorld import hello
 from python.TestingLanguage.FunctionCall import add_numbers
 
 
 class Test(TestCase):
+    def test_hello(self):
+        actual = hello()
+        expected = "Hello World!!!"
+        self.assertEqual(actual, expected)
+
     def test_add_numbers(self):
         actual = add_numbers(1, 2)
         expected = 3

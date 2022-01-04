49ab216ac9d33b7f69d01d0d3891673de2e01078
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2022-01-04 19:27:56 +0100

    Fixing format and result

---
+++ python/TestingLanguage/SortNumbers.py
@@ -0,0 +1,10 @@
+
+
+number1 = input("Enter a number? ")
+number2 = input("Enter a number? ")
+number3 = input("Enter a number? ")
+
+number_list = [number1, number2, number3]
+number_list.sort()
+
+print(number_list[0], ",", number_list[1], ",", number_list[2])

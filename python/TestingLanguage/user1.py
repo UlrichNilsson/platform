b94c79148ca9c7d559a1775e310e9933f2db2a73
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2022-01-04 17:58:12 +0100

    Refactoring file to meet formatting rules

--- python/TestingLanguage/user1.py
+++ python/TestingLanguage/user1.py
@@ -1,14 +1,14 @@
-inputnum = [0,0,0]
+
+inputnum = [0, 0, 0]
     
-print ("Enter a number\n")
+print("Enter a number\n")
 inputnum[0] = int(input())
 
-print ("Enter a number\n")
+print("Enter a number\n")
 inputnum[1] = int(input())
 
-print ("Enter a number\n")
+print("Enter a number\n")
 inputnum[2] = int(input())
-
 
 
 inputnum.sort()

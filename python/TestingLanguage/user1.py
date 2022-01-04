5f0318b74efb89ee766e293e220b4798aa4eeb6b
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2022-01-04 19:38:01 +0100

    Fixing format and result

--- python/TestingLanguage/user1.py
+++ python/TestingLanguage/user1.py
@@ -1,16 +1,16 @@
 
-inputnum = [0, 0, 0]
+input_number = [0, 0, 0]
     
 print("Enter a number\n")
-inputnum[0] = int(input())
+input_number[0] = int(input())
 
 print("Enter a number\n")
-inputnum[1] = int(input())
+input_number[1] = int(input())
 
 print("Enter a number\n")
-inputnum[2] = int(input())
+input_number[2] = int(input())
 
 
-inputnum.sort()
+input_number.sort()
 
-print('sorted list=', inputnum)
+print('sorted list=', input_number)

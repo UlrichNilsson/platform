283c059a057823f9cc1b399421533d5bbf9974c8
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2022-01-04 19:15:20 +0100

    Fixing format and result

--- python/PythonForEveryone/ex_05/51-whileloop.py
+++ python/PythonForEveryone/ex_05/51-whileloop.py
@@ -2,23 +2,24 @@ largest = None
 smallest = None
 while True:
     num = input("Enter a number: ")
-    if num == "done" : break
+    if num == "done":
+        break
     try:
-        fnum=int(num)
+        fnum = int(num)
     except:
         print('Invalid input')
         continue
-    if(largest is None):
-        largest=fnum
+    if largest is None:
+        largest = fnum
     else:
-        if(fnum>largest):
-            largest=fnum
-            
-    if(smallest is None):
-        smallest=fnum
+        if fnum > largest:
+            largest = fnum
+
+    if  smallest is None:
+        smallest = fnum
     else:
-        if(fnum<smallest):
-            smallest=fnum   
+        if fnum < smallest:
+            smallest = fnum
 
 print("Maximum is", largest)
 print("Minimum is", smallest)

4882f96b12895fbebd8d0141b7674b2c0a4d233b
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2022-01-04 18:54:48 +0100

    Fixing format and result

--- python/DataScience/pythonCsvImport.py
+++ python/DataScience/pythonCsvImport.py
@@ -1,8 +1,8 @@
 import csv
 
-#%precision 2
+# %precision 2
 
 with open('mpg.csv') as csvfile:
     mpg = list(csv.DictReader(csvfile))
     
-mpg[:3] # The first three dictionaries in our list.
+print(mpg[:3])  # The first three dictionaries in our list.

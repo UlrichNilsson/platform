23d7b2d2d75b2c5eef0642d2077c2afe295210d2
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2022-01-26 19:52:29 +0100

    Basic service

--- Angular/course/Part-2/conFusion2/src/app/menu/menu.component.ts
+++ Angular/course/Part-2/conFusion2/src/app/menu/menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
-
+import { DishService } from '../services/dish.service';
  
 @Component({
   selector: 'app-menu',
@@ -11,17 +10,20 @@ import { DISHES } from '../shared/dishes';
 
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = DISHES;
+  dishes: Dish[]=[];
 
   selectedDish: Dish;
 
-  constructor() { 
+  constructor(private dishService: DishService) { 
 
     this.selectedDish=new Dish;
+
   }
 
-  ngOnInit(): void {
+  ngOnInit():void {
+    this.dishes = this.dishService.getDishes();
   }
+ 
 
   onSelect(dish: Dish){
 

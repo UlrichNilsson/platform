5ae29f042ff1c6535be686c5d433ea43c252f362
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2022-01-31 19:45:26 +0100

    Adding leader service

--- Angular/course/Part-2/conFusion2/src/app/about/about.component.ts
+++ Angular/course/Part-2/conFusion2/src/app/about/about.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  leader: Leader;
 
-  ngOnInit(): void {
-  }
+  constructor(private leaderservice: LeaderService,
+    private route: ActivatedRoute,
+    private location: Location) {
 
+  this.leader = new Leader();
+}
+
+ngOnInit() {
+  const id = +this.route.snapshot.params['id'];
+  this.leader = this.leaderservice.getLeader(id.toString());
+}
+
+goBack(): void {
+  this.location.back();
+}
 }

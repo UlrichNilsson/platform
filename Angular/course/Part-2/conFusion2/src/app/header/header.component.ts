04cdca5fb17e59315c0a373b06f2bdeb3bfa5337
Ulrich Nilsson <ulrich_nilsson@hotmail.com>
2022-02-03 20:00:24 +0100

    Start of Login dialog

--- Angular/course/Part-2/conFusion2/src/app/header/header.component.ts
+++ Angular/course/Part-2/conFusion2/src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+//import { MatDialog, MatDialogRef } from '@angular/material';
+//import { MatDialog } from '@angular/material';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-
   
+  constructor() { }
+  //constructor(public dialog: MatDialog ) { }
+
+   
   ngOnInit(): void {
+  }
+
+  openLoginForm() {
+    //this.dialog.open(LoginComponent, {width: '500px', height: '450px'});
   }
 
 }
